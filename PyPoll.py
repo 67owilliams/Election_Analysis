969c1156364765ab1f5522b0dcea9fbc61fd21e5
67owilliams <67owilliams@gmail.com>
2020-10-17 18:55:02 -0500

    Created outline for script  with psuedocode

---
+++ PyPoll.py
@@ -0,0 +1,6 @@
+#The data that needs to be retrieved
+#1.The toal number of votes cast
+#2.A complete list of candidates that received votes
+#3.The percentage of votes each candidate won
+#4.The total number of votes each candidate won
+#5.The winner of the election based on popular vote
