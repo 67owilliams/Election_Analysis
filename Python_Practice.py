f27c6bdfc8c8638f633dcd9ef3c45d5ad5f7df4c
67owilliams <67owilliams@gmail.com>
2020-10-17 18:14:50 -0500

    Added comments

--- Python_Practice.py
+++ Python_Practice.py
@@ -19,3 +19,11 @@ if "Arapahoe" in counties or "El Paso" in counties:
     print("Arapahoe or El Paso is in the list of counties.")
 else:
     print("Arapahoe and El Paso are not in the list of counties.")
+
+#Loop through list with 'for'
+for county in counties:
+    print(county)
+
+#Loop through list using range of list
+for i in range(len(counties)):
+    print(counties[i])
