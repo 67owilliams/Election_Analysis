bfe44e820491937569795b57d610eb1bfb5262e9
67owilliams <67owilliams@gmail.com>
2020-10-17 21:40:47 -0500

    Added script to read csv

--- PyPoll.py
+++ PyPoll.py
@@ -1,29 +1,26 @@
-#The data that needs to be retrieved
 import csv
 import os
-#file_to_load = "C:\Users\odas4\Desktop\Class_Folder\Module_3\Resources\election_results.csv"
+
+#Set variable to path to open
 file_to_load = os.path.join("Resources", "election_results.csv")
 
-#Open CSV file
-#election_data = open(file_to_load, "r")
-with open(file_to_load) as election_data:
-    print(election_data)
-
-# Create a filename variable to a direct or indirect path to the file.
+#Set variable to path to save
 file_to_save = os.path.join("Analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the file.
-open(file_to_save, "w")
 
-# Using the with statement open the file as a text file.
-outfile = open(file_to_save, "w")
-# Write some data to the file.
-outfile.write("Hello World")
-
-#1.The toal number of votes cast
-#2.A complete list of candidates that received votes
-#3.The percentage of votes each candidate won
-#4.The total number of votes each candidate won
-#5.The winner of the election based on popular vote
+#Open election_results.csv to read
+with open(file_to_load) as election_data:
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+    
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
+    
+    #1.The toal number of votes cast
+    #2.A complete list of candidates that received votes
+    #3.The percentage of votes each candidate won
+    #4.The total number of votes each candidate won
+    #5.The winner of the election based on popular vote
 
 #Close CSV file
 election_data.close()
