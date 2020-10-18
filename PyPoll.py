a53f4ca66c2686d818a914a4c836e0ea471a4901
67owilliams <67owilliams@gmail.com>
2020-10-18 00:09:23 -0500

    Edited PyPoll to write to election_analysis.txt

--- PyPoll.py
+++ PyPoll.py
@@ -48,6 +48,18 @@ with open(file_to_load) as election_data:
         #Count candidate votes
         candidate_votes[candidate_name] += 1
 
+# Save the results to our text file.
+with open(file_to_save, "w") as txt_file:
+    # Print the final vote count to the terminal.
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    # Save the final vote count to the text file.
+    txt_file.write(election_results)
+
     #3.The percentage of votes each candidate won
     #Iterate through the candidate list.
     for candidate_name in candidate_votes:
@@ -56,10 +68,9 @@ with open(file_to_load) as election_data:
         #Calculate the percentage of votes.
         vote_percentage = float(votes) / float(total_votes) * 100
         #Print the candidate name and percentage of votes.
-        print(f"{candidate_name}: received {vote_percentage}% of the vote.")
-
-        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-        
+        #print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+        #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            
         #4.The total number of votes each candidate won
         #Determine if the votes are greater than the winning count.
         if (votes > winning_count) and (vote_percentage > winning_percentage):
@@ -70,14 +81,14 @@ with open(file_to_load) as election_data:
             #Set the winning_candidate equal to the candidate's name.
             winning_candidate = candidate_name
 
-    #5.The winner of the election based on popular vote
-    winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-    print(winning_candidate_summary)
+        #5.The winner of the election based on popular vote
+        winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    #print(winning_candidate_summary)
 
 #Close CSV file
 election_data.close()
